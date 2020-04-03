ed075e54036f11a258f18a43f607ceb75ffcbedf
Kenneth <kenneth.lynne@gmail.com>
2020-04-03 09:50:43 +0200

    chore: improve test

---
+++ apps/mobile/__tests__/App-test.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { act, create } from 'react-test-renderer';
+import App from '../App';
+
+jest.mock('expo', () => ({
+  Linking: {
+    makeUrl: () => '/',
+  },
+  SplashScreen: {
+    preventAutoHide: () => 'preventAutoHide',
+    hide: () => 'hide',
+  },
+}));
+
+describe('App', () => {
+  jest.useFakeTimers();
+
+  let tree;
+  it(`renders correctly`, async () => {
+    // act is used to prevent snapshot returning null
+    await act(async () => {
+      tree = await create(<App />);
+    });
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
